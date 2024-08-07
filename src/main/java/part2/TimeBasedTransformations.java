2fca36b6a166fb0a02aa16c94545168c8de03eb7
Ravi Subramanian - r0s0fin <Ravishankar.Subraman@walmart.com>
2024-08-07 16:42:35 -0500

    Window functions completed.

---
+++ src/main/java/part2/TimeBasedTransformations.java
@@ -0,0 +1,14 @@
+package part2;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class TimeBasedTransformations {
+
+    static StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/part2/WindowFunctions.java
+++ src/main/java/part2/WindowFunctions.java
@@ -1,20 +1,34 @@
 package part2;
 
-import gaming.*;
+import com.google.common.collect.Iterables;
+import gaming.Player;
+import gaming.PlayerRegistered;
+import gaming.ServerEvent;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.streaming.api.datastream.AllWindowedStream;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.KeyedStream;
+import org.apache.flink.streaming.api.datastream.WindowedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.windowing.AllWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.EventTimeSessionWindows;
+import org.apache.flink.streaming.api.windowing.assigners.GlobalWindows;
+import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.triggers.CountTrigger;
+import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.util.Collector;
 
 import java.time.Duration;
 import java.time.Instant;
-import java.util.Collection;
 import java.util.List;
 import java.util.UUID;
 
@@ -53,10 +67,13 @@ public class WindowFunctions {
     );
 
     static DataStream<ServerEvent> eventStream = env.fromCollection(events, TypeInformation.of(ServerEvent.class))
-            .assignTimestampsAndWatermarks( // extract timestamps for events (event time) + watermarks
-                WatermarkStrategy<ServerEvent>.forBoundedOutOfOrderness(
-                        Duration.ofMillis(500) // once you get an event with times T, you will NOT accept further events with time < T - 500
-                )
+            .assignTimestampsAndWatermarks(WatermarkStrategy.<ServerEvent>forBoundedOutOfOrderness(Duration.ofSeconds(1))
+                    .withTimestampAssigner(new SerializableTimestampAssigner<ServerEvent>() {
+                        @Override
+                        public long extractTimestamp(ServerEvent element, long recordTimestamp) {
+                            return element.getEventTime().toEpochMilli();
+                        }
+                    })
             );
 
     // how many players were registered every 3 seconds
@@ -97,9 +114,178 @@ public class WindowFunctions {
         env.execute();
     }
 
+    static class CountByWindowV2 extends ProcessAllWindowFunction<ServerEvent, String, TimeWindow> {
+        @Override
+        public void process(ProcessAllWindowFunction<ServerEvent, String, TimeWindow>.Context context, Iterable<ServerEvent> iterable, Collector<String> collector) throws Exception {
+            // Get time window from context
+            TimeWindow window = context.window();
+
+            int eventCount = 0;
+            for (ServerEvent event : iterable) if (event instanceof PlayerRegistered)
+                eventCount += 1;
+
+            collector.collect(
+                    "[" + window.getStart() + " - " + window.getEnd() + "] - " + eventCount + " players registered"
+            );
+        }
+    }
+
+    public static void demoCountByWindowV2() throws Exception {
+        AllWindowedStream<ServerEvent, TimeWindow> threeSecondsTumblingWindow = eventStream.windowAll(
+                TumblingEventTimeWindows.of(Time.seconds(3))
+        );
+
+        DataStream<String> registrationEvery3Secs = threeSecondsTumblingWindow.process(new CountByWindowV2());
+
+        registrationEvery3Secs.print();
+        env.execute();
+    }
+
+    // alternative #2: aggregate function
+    static class CountByWindowV3 implements AggregateFunction<ServerEvent, Long, Long> {
+        //                                                    ^input       ^acc  ^output
+        // start counting at 0
+        @Override
+        public Long createAccumulator() {
+            return 0L;
+        }
+
+        // every element increases accumulator by 1
+        @Override
+        public Long add(ServerEvent value, Long accumulator) {
+            if (value instanceof PlayerRegistered)
+                return accumulator + 1;
+            return accumulator;
+        }
+
+        // push a final output out of the final accumulator
+        @Override
+        public Long getResult(Long accumulator) {
+            return accumulator;
+        }
+
+        // accum1 + accum2 = a bigger accumulator
+        @Override
+        public Long merge(Long a, Long b) {
+            return a + b;
+        }
+    }
+
+    public static void demoCountByWindowV3() throws Exception {
+        AllWindowedStream<ServerEvent, TimeWindow> threeSecondsTumblingWindow = eventStream.windowAll(
+                TumblingEventTimeWindows.of(Time.seconds(3))
+        );
+
+        DataStream<Long> registrationEvery3Secs = threeSecondsTumblingWindow.aggregate(new CountByWindowV3());
+
+        registrationEvery3Secs.print();
+        env.execute();
+    }
+
+    /**
+     * Keyed streams and window functions
+     */
+
+    // each element will be assigned to a "mini-stream" for its own key
+    static KeyedStream<ServerEvent, String> streamByType = eventStream.keyBy(e -> e.getClass().getSimpleName());
+
+    // for every key, we'll have a separate window allocation
+    static WindowedStream<ServerEvent, String, TimeWindow> threeSecondsTumblingWindowsByType = streamByType
+            .window(TumblingEventTimeWindows.of(Time.seconds(3)));
+
+    static class CountInWindow implements WindowFunction<ServerEvent, String, String, TimeWindow> {
+        @Override
+        public void apply(String s, TimeWindow timeWindow, Iterable<ServerEvent> iterable, Collector<String> collector) throws Exception {
+            collector.collect(s + ": " + timeWindow + ", " + Iterables.size(iterable));
+        }
+
+    }
+
+    public static void demoCountByTypeByWindow() throws Exception {
+        DataStream<String> finalStream = threeSecondsTumblingWindowsByType.apply(new CountInWindow());
+        finalStream.print();
+        env.execute();
+    }
+
+    // alternative: process function for windows
+    static class CountInWindowV2 extends ProcessWindowFunction<ServerEvent, String, String, TimeWindow> {
+        @Override
+        public void process(String s, ProcessWindowFunction<ServerEvent, String, String, TimeWindow>.Context context, Iterable<ServerEvent> iterable, Collector<String> collector) throws Exception {
+            collector.collect(s + ": " + context.window() + ", " + Iterables.size(iterable));
+        }
+    }
+
+    public static void demoCountByTypeByWindowV2() throws Exception {
+        DataStream<String> finalStream = threeSecondsTumblingWindowsByType.process(new CountInWindowV2());
+        finalStream.print();
+        env.execute();
+    }
+
+    // one task processes all the data for a particular key
+
+    /**
+     * Sliding windows
+     */
+
+    // how many players were registered every 3 seconds, UPDATED EVERY 1s?
+    // [0..3s] [1..4s] [2..5s]
+
+    public static void demoSlidingAllWindows() throws Exception {
+        Time windowSize = Time.seconds(3);
+        Time slidingTime = Time.seconds(1);
+
+        AllWindowedStream<ServerEvent, TimeWindow> slidingWindowsAll = eventStream
+                .windowAll(SlidingEventTimeWindows.of(windowSize, slidingTime));
+
+        // process the windowed streams with similar window function
+        DataStream<String> registrationCountByWindow = slidingWindowsAll.apply(new CountByWindowAll());
+        registrationCountByWindow.print();
+        env.execute();
+    }
+
+    /**
+     * Session windows = group of events with NO MORE THAN a certain time gap in between them
+     */
+    // how many registration events do we have no more than 1 second apart
+    public static void demoSessionWindows() throws Exception {
+        AllWindowedStream<ServerEvent, TimeWindow> groupBySessionWindows = eventStream
+                .windowAll(EventTimeSessionWindows.withGap(Time.seconds(1)));
+
+        DataStream<String> countBySessionWindows = groupBySessionWindows.apply(new CountByWindowAll());
+        countBySessionWindows.print();
+        env.execute();
+    }
+
+    /**
+     * Global window
+     */
+    // how many registration events do we have every 10 events
+
+    static class CountByGlobalWindow implements AllWindowFunction<ServerEvent, String, GlobalWindow> {
+        //                                                            ^ input      ^ output  ^ window type
+        @Override
+        public void apply(GlobalWindow window, Iterable<ServerEvent> values, Collector<String> out) throws Exception {
+            int eventCount = 0;
+            for (ServerEvent e : values) if (e instanceof PlayerRegistered)
+                eventCount += 1;
+
+            out.collect(
+                    eventCount + " players registered"
+            );
+        }
+    }
+
+    public static void demoGlobalWindow() throws Exception {
+        DataStream<String> globalWindowEvents = eventStream.windowAll(GlobalWindows.create())
+                .trigger(CountTrigger.of(10L))
+                .apply(new CountByGlobalWindow());
+
+        globalWindowEvents.print();
+        env.execute();
+    }
 
     public static void main(String[] args) throws Exception {
-        demoCountByWindow();
+        demoGlobalWindow();
     }
 
 }
